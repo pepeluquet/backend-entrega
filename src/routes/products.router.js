02ce1473c01ca0f14e4345828cac12b7c25f21bc
Christian Luquet <pepeluquet@hotmail.com>
2025-09-27 23:31:16 -0300

    multer

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -4,6 +4,7 @@ const config = require('../config/config.js');
 const ProductsDao = require(`../dao/products.dao.js`)
 const ProductsServices = require('../services/products.services.js')
 const ProductsControllers = require('../controllers/products.controllers.js')
+const uploader = require('../utils/uploaders.js')
 
 const productsDao = new ProductsDao()
 const productsServices = new ProductsServices(productsDao)
@@ -16,8 +17,21 @@ router.get('/', productsControllers.getAllProducts)
 // GET /api/products/:pid
 router.get('/:pid', productsControllers.getProductById)
 
-// POST /api/products
-router.post('/', productsControllers.createProduct)
+// POST /api/products (con Multer)
+router.post(
+  '/',
+  uploader.array('thumbnails'),
+  async (req, res) => {
+    try {
+      // Procesa las rutas de las imágenes subidas
+      const thumbnails = req.files ? req.files.map(file => `/images/${file.filename}`) : [];
+      // Envía el body y las rutas al controlador
+      await productsControllers.createProductWithImages(req, res, thumbnails);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
 
 // PUT /api/products/:pid
 router.put('/:pid', productsControllers.updateProduct)
